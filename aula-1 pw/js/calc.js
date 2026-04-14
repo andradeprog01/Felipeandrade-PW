d37c1a6494778b2295616575c569eb6c28d2832c
andradeprog01 <felipeandrade13012010@gmail.com>
2026-04-14 08:04:17 -0300

    Add files via upload

---
+++ aula-1 pw/js/calc.js
@@ -0,0 +1,11 @@
+var num1 = parseInt(prompt("Entre com o primeiro número: ")) 
+var num2 = parseInt(prompt("Entre com o segundo número: ")) 
+var soma = (num1+num2);
+var sub = (num1-num2);
+var mult = (num1*num2)
+var div = (num1/num2)
+
+console.log("A soma dos 2 números é: "+soma)
+console.log("O primeiro número subtraido pelo segundo resulta em: "+sub)
+console.log("A multiplicação dos números resulta em: "+mult)
+console.log("o primeiro número dividivo pelo segundo é "+div)
