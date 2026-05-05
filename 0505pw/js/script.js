2805903f22b6f6f33e44699a4079203b202de999
felipe <felipeandrade13012010@gmail.com>
2026-05-05 08:09:26 -0300

    aula pw 05/05

---
+++ 0505pw/js/script.js
@@ -0,0 +1,25 @@
+// Estrutura de Repetição while
+
+    var x = 0;
+    var soma = 0;
+while (x<5)
+{
+   x++
+   soma= soma+x;
+   console.log(soma)
+}
+// criar array passando por todos os elementos
+var arr = ['teste1','teste2','teste3','a']
+var y = 0;
+
+console.log (arr)
+//passando por todos as palavras
+var palavra = "FELIPE";
+var i = 0;
+while(i<6)
+{
+    console.log(palavra[i]);
+    i++
+}
+
+
