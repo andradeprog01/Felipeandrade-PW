3558749927f37781f56104b3fb184dc58420d461
Felipe <felipeandrade13012010@gmail.com>
2026-06-16 08:09:45 -0300

    Adicionando total a loja

--- lojapw/js/script.js
+++ lojapw/js/script.js
@@ -9,11 +9,28 @@ function atualizarTotal(linha) {
   linha.querySelector(".total").textContent = "R$ " + total.toFixed(2).replace(".", ",");
 }
 
+function atualizarSubtotalGeral() {
+  let somaTotal = 0;
+  
+  document.querySelectorAll(".total").forEach(function(elementoTotal) {
+    let texto = elementoTotal.textContent
+      .replace("R$ ", "")
+      .replace(/\./g, "")
+      .replace(",", ".");
+    
+    somaTotal += parseFloat(texto) || 0;
+  });
+  
+
+  document.getElementById("subtotal").textContent = "R$ " + somaTotal.toFixed(2).replace(".", ",");
+}
+
 document.querySelectorAll(".btn-mais").forEach(function(btn) {
   btn.addEventListener("click", function() {
     let span = btn.previousElementSibling;
     span.textContent = parseInt(span.textContent) + 1;
     atualizarTotal(btn.closest("tr"));
+    atualizarSubtotalGeral(); 
   });
 });
 
@@ -23,6 +40,7 @@ document.querySelectorAll(".btn-menos").forEach(function(btn) {
     if (parseInt(span.textContent) > 0) {
       span.textContent = parseInt(span.textContent) - 1;
       atualizarTotal(btn.closest("tr"));
+      atualizarSubtotalGeral(); 
     }
   });
 });
